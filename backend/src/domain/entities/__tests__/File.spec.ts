e6962213970a92404a79b2b3e4791c8a12ad8470
Eduardo Carvalho <educarvalho009@gmail.com>
2023-11-18 07:38:47 -0400

    test: match File case

---
+++ backend/src/domain/entities/__tests__/File.spec.ts
@@ -0,0 +1,28 @@
+import { File } from "../File";
+
+describe('File interface', () => {
+    it('should match the File interface structure', () => {
+      const file: File = {
+        fieldname: 'file',
+        originalname: 'example.csv',
+        encoding: 'utf-8',
+        mimetype: 'text/csv',
+        buffer: Buffer.from('example csv data'),
+        size: 1000,
+      };
+  
+      expect(file).toHaveProperty('fieldname');
+      expect(file).toHaveProperty('originalname');
+      expect(file).toHaveProperty('encoding');
+      expect(file).toHaveProperty('mimetype');
+      expect(file).toHaveProperty('buffer');
+      expect(file).toHaveProperty('size');
+      expect(typeof file.fieldname).toBe('string');
+      expect(typeof file.originalname).toBe('string');
+      expect(typeof file.encoding).toBe('string');
+      expect(typeof file.mimetype).toBe('string');
+      expect(file.buffer).toBeInstanceOf(Buffer);
+      expect(typeof file.size).toBe('number');
+    });
+  });
+  
