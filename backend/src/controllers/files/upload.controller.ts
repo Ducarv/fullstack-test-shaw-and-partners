87609a68b1b950832721c91ee420d1ae705a2055
Eduardo Carvalho <educarvalho009@gmail.com>
2023-11-17 22:15:04 -0400

    refactor: upload file

--- backend/src/controllers/files/upload.controller.ts
+++ backend/src/controllers/files/upload.controller.ts
@@ -1,17 +1,24 @@
 import { Request, Response } from "express";
 import { UploadFileUseCase } from "../../domain/useCases/uploadFile";
+import { File } from "../../domain/entities/File";
 
 export class UploadFileController {
     constructor(private uploadFileUseCase: UploadFileUseCase) {}
 
     async handle(request: Request, response: Response) {
-        const { file } = request.body;
+        const file = request.file;
+
+        if (!file) {
+            return response.status(400).json({ message: "No file uploaded." });
+        }
 
         try {
             const uploadedFile = await this.uploadFileUseCase.execute(file);
             response.status(201).json(uploadedFile);
         } catch(error: unknown) {
-            response.status(400).json(error);
+            if(error instanceof Error) {
+                response.status(400).json(error.message);
+            }
         }
     }
 }
--- backend/src/controllers/routes/routes.ts
+++ backend/src/controllers/routes/routes.ts
@@ -1,8 +1,9 @@
 import { Request, Response, Router } from "express";
 import { uploadFileController } from "../files";
+import { multerConfig } from "../../providers/multer";
 
 export const router = Router();
 
-router.post('/api/files', async (request: Request, response: Response) => {
-    await uploadFileController.handle(request, response); 
+router.post('/api/files', multerConfig.single('file'), async (request: Request, response: Response) => {
+   await uploadFileController.handle(request, response); 
 })
