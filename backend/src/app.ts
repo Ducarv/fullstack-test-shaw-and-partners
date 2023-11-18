a8e94c7a097e80f66c54064088caf1af2a961cb6
Eduardo Carvalho <educarvalho009@gmail.com>
2023-11-17 22:18:04 -0400

    fix/add: dependencies and app file

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,6 +6,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}))
+
 app.use(router)
 
 app.listen(PORT, () => {
