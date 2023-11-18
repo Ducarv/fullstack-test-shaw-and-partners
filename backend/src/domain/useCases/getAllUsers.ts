8166a016fb48fcef44e63c371b626398ccfc6956
Eduardo Carvalho <educarvalho009@gmail.com>
2023-11-18 08:27:37 -0400

    refactor: getAllUsers use case

--- backend/src/domain/useCases/getAllUsers.ts
+++ backend/src/domain/useCases/getAllUsers.ts
@@ -5,8 +5,23 @@ export class GetAllUsersUseCase {
 
     async execute(query?: QueryType) {
         try {
-            const users = await this.repository.getAll(query);
-            return users;
+            if (query) {
+                const allUsers = await this.repository.getAll();
+
+                const filteredUsers = allUsers?.filter((user) => {
+                    return (
+                        user.name.toLowerCase().includes(query.toLowerCase()) ||
+                        user.city.toLowerCase().includes(query.toLowerCase()) ||
+                        user.country.toLowerCase().includes(query.toLowerCase()) ||
+                        user.favorite_sport.toLowerCase().includes(query.toLowerCase())
+                    );
+                });
+
+                return filteredUsers;
+            } else {
+                const allUsers = await this.repository.getAll();
+                return allUsers;
+            }
         } catch(error: unknown) {
             if (error instanceof Error) {
                 throw new Error(error.message);
--- backend/src/repository/userRepository.ts
+++ backend/src/repository/userRepository.ts
@@ -3,6 +3,6 @@ import { User } from "../domain/entities/User";
 export type QueryType = string | undefined;
 
 export interface UserRepository {
-    getAll: (query: QueryType) => Promise<User[] | undefined>;
+    getAll: (query?: QueryType) => Promise<User[] | undefined>;
     create: (users: User) => Promise<User | undefined>;
 }
