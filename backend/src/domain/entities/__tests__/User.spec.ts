8253fce0674af38fc0b883fcfda1647ad1d8b341
Eduardo Carvalho <educarvalho009@gmail.com>
2023-11-18 07:53:40 -0400

    test: user interface

---
+++ backend/src/domain/entities/__tests__/User.spec.ts
@@ -0,0 +1,37 @@
+import { User } from "../User"
+
+describe("User interface", () => {
+    it('should match the User interface structure', () => {
+        const user: User = {
+            name: "example1",
+            city: "New York",
+            country: "USA",
+            favorite_sport: "Soccer",
+        }
+
+        expect(user).toHaveProperty("name");
+        expect(user).toHaveProperty("city");
+        expect(user).toHaveProperty("country");
+        expect(user).toHaveProperty("favorite_sport");
+        expect(typeof user.name).toBe('string');
+        expect(typeof user.city).toBe('string');
+        expect(typeof user.country).toBe('string');
+        expect(typeof user.favorite_sport).toBe('string');
+    });
+
+    it('should throw an error if the file structure is incorrect', () => {
+        const invalidUser: Partial<User> = {
+            name: "example1",
+            city: "New York",
+            favorite_sport: "Soccer",
+        }
+
+        function validateUserStructure() {
+            if(!invalidUser.country) {
+                throw new Error('User structure  is incorrect!');
+            }
+        }
+
+        expect(validateUserStructure).toThrow('User structure  is incorrect!')
+    })
+})
