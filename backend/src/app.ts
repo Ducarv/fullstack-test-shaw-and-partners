d964c3fdab5342810459ae8004cc52bf8303aa07
Eduardo Carvalho <educarvalho009@gmail.com>
2023-11-18 11:34:48 -0400

    config: cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import 'dotenv/config'
 import { router } from "./controllers/routes/routes";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors())
 app.use(express.urlencoded({
     extended: true
 }))
