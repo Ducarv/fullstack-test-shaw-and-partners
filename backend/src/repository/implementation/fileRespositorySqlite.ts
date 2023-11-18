77b925c57f29e592e67f7a9dad2ae69382ec84cb
Eduardo Carvalho <educarvalho009@gmail.com>
2023-11-17 22:17:30 -0400

    refactor: file sqlite implementation fix

--- backend/src/repository/implementation/fileRespositorySqlite.ts
+++ backend/src/repository/implementation/fileRespositorySqlite.ts
@@ -1,12 +1,55 @@
+import { Readable } from "stream";
+import readLine from "readline";
 import { File } from "../../domain/entities/File";
 import { prisma } from "../../infra/database/prisma/prisma";
+import { User } from "../../domain/entities/User";
 
 export class FileRepositorySQLite {
     async upload(data: File) {
         try {
+            if (!data) {
+                throw new Error("Data is missing");
+            }
+
             const savedFile = await prisma.file.create({
-                data: data as File
+                data: {
+                    ...data
+                }
             })
+
+            const { buffer } = savedFile;
+
+            const readbleFile = new Readable();
+            readbleFile.push(buffer);
+            readbleFile.push(null);
+
+            const usersLine = readLine.createInterface({
+                input: readbleFile
+            })
+
+            const users: User[] = [];
+
+            for await (let user of usersLine) {
+                const userLineSplit = user.split(",")
+
+                users.push({
+                    name: userLineSplit[0],
+                    city: userLineSplit[1],
+                    country: userLineSplit[2],
+                    favorite_sport: userLineSplit[3]
+                })
+            }
+
+            for await (let { name, city, country, favorite_sport } of users) {
+                await prisma.user.create({
+                   data: {
+                    name,
+                    city,
+                    country,
+                    favorite_sport
+                   } 
+                })
+            } 
     
             return savedFile;
         } catch(error: unknown) {
