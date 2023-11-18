04e2a0a1eb2f7118c21043ab5ed8708e1d4e20d1
Eduardo Carvalho <educarvalho009@gmail.com>
2023-11-17 23:09:59 -0400

    delete: multer file commets

--- backend/src/providers/multer.ts
+++ backend/src/providers/multer.ts
@@ -1,17 +1,3 @@
 import multer from "multer";
-//import path from "path";
-
-// const storage = multer.diskStorage({
-//     destination: (req, file, callback) => {
-//         callback(null, '../../public/uploads/')
-//     },
-//     filename: (req, file, callcack) => {
-//         callcack(
-//             null,
-//             file.fieldname + '-' + Date.now() + path.extname(file.originalname)
-//         )
-//     }
-// })
 
 export const multerConfig = multer();
-//export const upload = multer({storage})
