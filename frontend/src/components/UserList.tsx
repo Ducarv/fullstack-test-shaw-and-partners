395bc5bb64b6fcb8bc847b5c34069a8e6e88e227
Eduardo Carvalho <educarvalho009@gmail.com>
2023-11-18 16:45:30 -0400

    style: page styles

--- frontend/src/components/UserList.tsx
+++ frontend/src/components/UserList.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react';
-import { fetchUsers } from '../providers/fetchUsers';
-import './UserList.css'
+import { useEffect, useState } from "react";
+import { fetchUsers } from "../providers/fetchUsers";
+import "./UserList.css";
 
 interface User {
   name: string;
@@ -10,41 +10,51 @@ interface User {
 }
 
 export const UserList = () => {
-    const [users, setUsers] = useState<User[]>([]);
-    const [isLoading, setIsLoading] = useState(false);
-  
-    useEffect(() => {
-      (async () => {
-        try {
-          setIsLoading(true);
-          const fetchedUsers = await fetchUsers();
-          setUsers(fetchedUsers);
-          setIsLoading(false);
-        } catch (error) {
-          console.error('Error to get users:', error);
-          setIsLoading(false);
-        }
-      })();
-    }, []);
-  
-    return (
-      <div>
-        <h2>User List</h2>
-        {isLoading ? <p>Loading users...</p> : (
-          <ul>
-            {users.map((user, index) => (
-              <li 
-                key={index}
-                className='user-card'
-              >
-                <p className="user-name">Name: {user.name}</p>
-                <p className="user-city">City: {user.city}</p>
-                <p className="user-country">Country: {user.country}</p>
-                <p className="user-favorite-sport">Favorite Sport: {user.favorite_sport}</p>
-              </li>
-            ))}
-          </ul>
-        )}
+  const [users, setUsers] = useState<User[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setIsLoading(true);
+        const fetchedUsers = await fetchUsers(searchTerm);
+        setUsers(fetchedUsers);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error to get users:", error);
+        setIsLoading(false);
+      }
+    })();
+  }, [searchTerm]);
+
+  return (
+    <div>
+      <h2>User List</h2>
+      <div className="search-bar">
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)}
+          placeholder="Search by name, city, country, or favorite sport"
+        />
       </div>
-    );
+      {isLoading ? (
+        <p>Loading users...</p>
+      ) : (
+        <ul>
+          {users.map((user, index) => (
+            <li key={index} className="user-card">
+              <p className="user-name">Name: {user.name}</p>
+              <p className="user-city">City: {user.city}</p>
+              <p className="user-country">Country: {user.country}</p>
+              <p className="user-favorite-sport">
+                Favorite Sport: {user.favorite_sport}
+              </p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 };
--- frontend/src/providers/fetchUsers.ts
+++ frontend/src/providers/fetchUsers.ts
@@ -1,18 +1,19 @@
 import { api } from "../api/axios";
 
-export const fetchUsers = async () => {
-    try {
-       const response = await api.get('/api/users');
-       const result = response.data;
-   
-       if (result && Array.isArray(result)) {
-         return result;
-       } else {
-         console.error('Invalid data fetched from API');
-         return [];
-       }
-    } catch (error) {
-       console.error('Error to get users:', error);
-       return [];
+export const fetchUsers = async (searchTerm?: string) => {
+  try {
+    const query = searchTerm ? `?q=${searchTerm}` : '';
+    const response = await api.get(`/api/users${query}`);
+    const result = response.data;
+
+    if (result && Array.isArray(result)) {
+      return result;
+    } else {
+      console.error('Invalid data fetched from API');
+      return [];
     }
+  } catch (error) {
+    console.error('Error to get users:', error);
+    return [];
+  }
 };
