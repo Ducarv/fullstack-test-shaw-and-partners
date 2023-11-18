4778013e26ff4929c009f0bf12b11e471b3d86a5
Eduardo Carvalho <educarvalho009@gmail.com>
2023-11-17 22:57:44 -0400

    refactor: fileRepository dependence

--- backend/src/controllers/files/index.ts
+++ backend/src/controllers/files/index.ts
@@ -1,8 +1,10 @@
 import { UploadFileUseCase } from "../../domain/useCases/uploadFile";
 import { FileRepositorySQLite } from "../../repository/implementation/fileRespositorySqlite";
+import { UserRepositorySQLite } from "../../repository/implementation/userRepositorySqlite";
 import { UploadFileController } from "./upload.controller";
 
-const fileRepositorySQLite = new FileRepositorySQLite();
+const userRepository = new UserRepositorySQLite()
+const fileRepositorySQLite = new FileRepositorySQLite(userRepository);
 const uploadFileUseCase = new UploadFileUseCase(fileRepositorySQLite);
 const uploadFileController = new UploadFileController(uploadFileUseCase);
 
